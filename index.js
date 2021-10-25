a5a7170db60767bda2f763abceabd041dc488885
courtney long <courtneylong877@gmail.com>
2021-10-25 11:11:22 -0400

    Split up the inquirer prompts to prevent having to sort through the inquirer questions

--- index.js
+++ index.js
@@ -4,92 +4,139 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 
-class Questions {
+
 
 firstMember() {
     inquirer
-    .prompt(
+    .prompt([
         {
             type: "input",
             name: "mananger",
             message: "What is the name of your team manager?"
-        });
-    then.addAMember();
-}
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is their email address?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is their github username?"
+        },
+        {
+            type: "input",
+            name: "number",
+            message: "What is their office number?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is their employee ID?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is their email address?"
+        },
+        {
+            type: "list",
+            name: "another",
+            message: "Would you like to...",
+            choices: ["Add an engineer?", "Add an intern?", "Finish building your team?"]
+        }
+])}
 
-anotherEngineer() {
-    inquirer
-    .prompt(
-    {
-        type: "input",
-        name: "engineer",
-        message: "What is the name of the Engineer?"}
-    )};
-
-anotherIntern() {
-    inquirer
-    .prompt(
-    {
-        type: "input",
-        name: "intern",
-        message: "What is the name of the Intern?"
-    })
-};
-    
-addAMember() {
+engineer() {
     inquirer
     .prompt([
-    {
-        type: "input",
-        name: "email",
-        message: "What is their email address?"
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "What is their github username?"
-    },
-    {
-        type: "input",
-        name: "number",
-        message: "What is their office number?"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is their employee ID?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is their email address?"
-    }
-])
-then.anotherMember();
-};
+        {
+            type: "input",
+            name: "engineer",
+            message: "What is the name of the Engineer?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is their email address?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is their github username?"
+        },
+        {
+            type: "input",
+            name: "number",
+            message: "What is their office number?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is their employee ID?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is their email address?"
+        },
+        {
+            type: "list",
+            name: "another",
+            message: "Would you like to...",
+            choices: ["Add an engineer?", "Add an intern?", "Finish building your team?"]
+        }
+])}
 
-anotherMember() {
+intern() {
     inquirer
-    .prompt(
-    {
-        type: "list",
-        name: "another",
-        message: "Would you like to...",
-        choices: ["Add an engineer?", "Add an intern?", "Finish building your team?"]
-    })
-    then.whatNext();
-};
+    .prompt([
+        {
+            type: "input",
+            name: "intern",
+            message: "What is the name of the Intern?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is their email address?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is their github username?"
+        },
+        {
+            type: "input",
+            name: "number",
+            message: "What is their office number?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is their employee ID?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is their email address?"
+        },
+        {
+            type: "list",
+            name: "another",
+            message: "Would you like to...",
+            choices: ["Add an engineer?", "Add an intern?", "Finish building your team?"]
+        }
+])}
 
-whatNext() {
-    if (anotherMember(answers.another)==="Add an engineer?") {
-        then.anotherEngineer()
-    } else if (anotherMember(answers.another)==="Add an intern?") {
-        then.anotherIntern()
-    } else if (anotherMember(answers.another)==="Finish building your team?") {
-        then.writeIt()
+if (answers.another === "Add an engineer?") {
+    this.firstMember()
+} else if (answers.another === "Add an intern?") {
+    this.firstMember()
 }
-}
-}
+
+
+
 
 
 
@@ -97,8 +144,7 @@ const addMember = require("./Questions/AddAMember");
 const writeHTML = util.promisify(fs.writeFile);
 
 const init = () => {
-    const questions = new Questions()
-    questions.firstMember()
+    firstMember()
     
 
 }
