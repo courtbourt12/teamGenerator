88f9bdb519a7d5639df52309a387098d015de6c7
courtney long <courtneylong877@gmail.com>
2021-10-25 13:35:40 -0400

    Added the HTML content

--- Questions/AddAMember.js
+++ Questions/AddAMember.js
@@ -1,70 +1,50 @@
+const generateHTML = (answers) => {
+`<!DOCTYPE html>
+<html lang="en">
 
-firstMember();
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CSS Snippet Cheatsheet</title>
+  <link rel="stylesheet" href="assets/css/reset.css" />
+  <link rel="stylesheet" href="./assets/css/style.css" />
+</head>
 
-const firstMember = () => {
-    return inquirer.prompt(
-    {
-        type: "input",
-        name: "mananger",
-        message: "What is the name of your team manager?"
-    })
-};
+<body>
+    <section class = "teamLeader"> 
+        <h1>Team Manager</h1>
+        <h2>${firstMember(answers.manager)}</h2>
+        <ul>
+            <li>Email: ${firstMember(answers.email)}</li>
+            <li>GitHub: ${firstMember(answers.github)}</li>
+            <li>Office Number: ${firstMember(answers.number)}</li>
+            <li>Employee ID: ${firstMember(answers.id)}</li>
+        </ul>
+    </section>
 
-const addAMember = () => {
-    return inquirer.prompt([
-    {
-        type: "input",
-        name: "teamMembers",
-        message: "What is the name of one of your team members?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is your team member's email address?"
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "What is your team member's github username?"
-    },
-    {
-        type: "input",
-        name: "number",
-        message: "What is your team member's office number?"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is your team member's employee ID?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is your team member's email address?"
-    }
-])
-anotherMember();
-};
+    <section class = "engineer"> 
+        <h1>Engineer</h1>
+        <h2>${engineer(answers.manager)}</h2>
+        <ul>
+            <li>Email: ${engineer(answers.email)}</li>
+            <li>GitHub: ${engineer(answers.github)}</li>
+            <li>Office Number: ${engineer(answers.number)}</li>
+            <li>Employee ID: ${engineer(answers.id)}</li>
+        </ul>
+    </section>
 
-const anotherMember = () => {
-    return inquirer.prompt(
-    {
-        type: "list",
-        name: "another",
-        message: "Would you like to add another team member?",
-        choices: ["Yes", "No"]
-    })
-};
-
-
-if (anotherMember().choices == "Yes") {
-
-    return inquirer.prompt(
-        {
-            type: "input",
-            name: "engineerIntern",
-            message: "Are you entering an engineer or an intern?"
-        });
-        
-    addAMember();
+    <section class = "intern"> 
+        <h1>Intern</h1>
+        <h2>${intern(answers.manager)}</h2>
+        <ul>
+            <li>Email: ${intern(answers.email)}</li>
+            <li>GitHub: ${intern(answers.github)}</li>
+            <li>Office Number: ${intern(answers.number)}</li>
+            <li>Employee ID: ${intern(answers.id)}</li>
+        </ul>
+    </section>
+</body>`
 }
+
+module.exports = generateHTML;
+
